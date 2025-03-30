659bdd506edc3e82d18e0de98eda5933dc7fd41b
HarshManiar1804 <harshmaniarofficial@gmail.com>
2025-03-30 19:52:16 +0530

    add loginmodel and auth options and route

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,14 +4,17 @@ import HeroSection from "@/components/base/HeroSection";
 import Navbar from "@/components/base/Navbar";
 import UserReviews from "@/components/base/UserReviews";
 import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOption, customSession } from "./api/auth/[...nextauth]/options";
 
-export default function Home() {
+export default async function Home() {
+  const sesstion: customSession | null = await getServerSession(authOption)
   return (
     <>
       <div className="min-h-screen flex flex-col ">
         {/* Header */}
-        <Navbar />
+        <Navbar user={sesstion?.user} />
         {/* Hero Section */}
         <HeroSection />
 
